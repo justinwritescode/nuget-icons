0c512c6483669133b48f00b199a9618f7d8f5315
Justin <justin@thebackroom.app>
2022-10-07 20:57:47 -0400

    Vesioning

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # nuget-icons
 A collection of randomly-colored NuGet icons
 
+## Usage
+
+
 ## Main Project
 Visit my [main project](https://github.com/justinwritescode).
--- src/nuget-icons/nuget-icons.cs
+++ src/nuget-icons/nuget-icons.cs
@@ -2,8 +2,16 @@ namespace JustinWritesCode.NuGet;
 
 public class GetRandomIcon : Microsoft.Build.Utilities.Task
 {
-    public static string GetRandomIcon(string key = null)
-    {
+    ITaskItem[] Icons { get; set; }
+    [Output]
+    ITaskItem OutputIcon { get; set; }
 
+    public bool Execute()
+    {
+        var random = new Random();
+        var index = random.Next(0, Icons.Length);
+        var icon = Icons[index];
+        Log.LogMessage("Selected icon: {0}", icon.ItemSpec);
+        return true;
     }
 }
